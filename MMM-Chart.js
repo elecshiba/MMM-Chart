233aaac0994c25fa24976af7aefb5ff2c76cde14
elecshiba <daisuke.elec@gmail.com>
2018-08-09 14:22:02 +0900

    [TEST] periodic updating

--- MMM-Chart.js
+++ MMM-Chart.js
@@ -9,9 +9,44 @@
 
 Module.register("MMM-Chart", {
     defaults: {
-        width       : 200,
-        height      : 200,
-        chartConfig : {}
+        width       : "200px",
+        height      : "200px",
+        chartConfig : {
+            type: 'bar',
+            data: {
+                labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+                datasets: [{
+                    label: '# of Votes',
+                    data: [12, 19, 3, 5, 2, 3],
+                    backgroundColor: [
+                        'rgba(255, 99, 132, 0.2)',
+                        'rgba(54, 162, 235, 0.2)',
+                        'rgba(255, 206, 86, 0.2)',
+                        'rgba(75, 192, 192, 0.2)',
+                        'rgba(153, 102, 255, 0.2)',
+                        'rgba(255, 159, 64, 0.2)'
+                    ],
+                    borderColor: [
+                        'rgba(255,99,132,1)',
+                        'rgba(54, 162, 235, 1)',
+                        'rgba(255, 206, 86, 1)',
+                        'rgba(75, 192, 192, 1)',
+                        'rgba(153, 102, 255, 1)',
+                        'rgba(255, 159, 64, 1)'
+                    ],
+                    borderWidth: 1
+                }]
+            },
+            options: {
+                scales: {
+                    yAxes: [{
+                        ticks: {
+                            beginAtZero:true
+                        }
+                    }]
+                }
+            }
+        }
     },
 
     getScripts: function() {
@@ -25,54 +60,29 @@ Module.register("MMM-Chart", {
 
 	start: function() {
         this.config = Object.assign({}, this.defaults, this.config);
-		Log.info("Starting module: " + this.name);
-	},
+        Log.info("Starting module: " + this.name);
+        
+        // Schedule update interval.
+		var self = this;
+		setInterval(function() {
+            self.fetchData();
+			self.updateDom();
+		}, 5000);
+    },
+    
+    fetchData: function() {
+        // Fetch data from Spread sheet, and update the config.
+        var numberArray = [];
+        for (var i = 0; i < 6; i+=1) {
+            numberArray.push(Math.floor(Math.random() * (100)));
+        }
+        this.defaults.chartConfig.datasets[0].data = numberArray;
+    },
 
 	getDom: function() {
         // Create wrapper element
         const wrapperEl = document.createElement("div");
         // wrapperEl.setAttribute("style", "position: relative; display: block; width: 200; height: 200;");
-
-        const _config = {
-            width       : "200px",
-            height      : "200px",
-            chartConfig : {
-                type: 'bar',
-                data: {
-                    labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
-                    datasets: [{
-                        label: '# of Votes',
-                        data: [12, 19, 3, 5, 2, 3],
-                        backgroundColor: [
-                            'rgba(255, 99, 132, 0.2)',
-                            'rgba(54, 162, 235, 0.2)',
-                            'rgba(255, 206, 86, 0.2)',
-                            'rgba(75, 192, 192, 0.2)',
-                            'rgba(153, 102, 255, 0.2)',
-                            'rgba(255, 159, 64, 0.2)'
-                        ],
-                        borderColor: [
-                            'rgba(255,99,132,1)',
-                            'rgba(54, 162, 235, 1)',
-                            'rgba(255, 206, 86, 1)',
-                            'rgba(75, 192, 192, 1)',
-                            'rgba(153, 102, 255, 1)',
-                            'rgba(255, 159, 64, 1)'
-                        ],
-                        borderWidth: 1
-                    }]
-                },
-                options: {
-                    scales: {
-                        yAxes: [{
-                            ticks: {
-                                beginAtZero:true
-                            }
-                        }]
-                    }
-                }
-            }
-        }
 
         // Create chart canvas
         const chartEl  = document.createElement("canvas");
@@ -84,7 +94,7 @@ Module.register("MMM-Chart", {
         chartEl.style.height = _config.height;
 
         // Init chart.js
-        this.chart = new Chart(chartEl.getContext("2d"), _config.chartConfig);
+        this.chart = new Chart(chartEl.getContext("2d"), this.defaults.chartConfig);
 
         // Append chart
         wrapperEl.appendChild(chartEl);
